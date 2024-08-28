fc28a80c5f645c505c65c8bb95a4208aaaf46f11
ebrarturudu <ebrartrd@gmail.com>
2024-08-28 15:51:28 +0300

    Service classlarının güncel son hali eklendi.

--- src/main/java/com/todo/service/CommentService.java
+++ src/main/java/com/todo/service/CommentService.java
@@ -1,15 +1,49 @@
 package com.todo.service;
 
-import com.todo.Comment;
+import com.todo.dto.CommentResponseDTO;
+import com.todo.entity.Comment;
 import com.todo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
-    private CommentRepository CommentRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-    public Comment createComment(Comment comment) {
-        return CommentRepository.save(comment);
+//    public Comment createComment(Comment comment) {
+//        return CommentRepository.save(comment);
+//    }
 
+    public Comment createComment(Comment commentDTO) {
+        Comment comment =new Comment();
+        comment.setId(commentDTO.getId());
+        comment.setText(commentDTO.getText());
+
+        commentRepository.save(comment);
+        return commentDTO;
+    }
+    @GetMapping("/comments/findall")
+
+    public List<CommentResponseDTO> findAll() {
+
+        return commentRepository.findAll().stream().map(comment -> {
+            CommentResponseDTO commentResponseDTO = new CommentResponseDTO();
+            commentResponseDTO.setText(comment.getText());
+            return commentResponseDTO;
+        }).collect(Collectors.toList());
+    }
+
+    public CommentResponseDTO findById(Long id) {
+        Comment comment = commentRepository.findById(id).get();
+
+        CommentResponseDTO commentResponseDTO = new CommentResponseDTO();
+        commentResponseDTO.setText(comment.getText());
+
+        return commentResponseDTO;
     }
 }
--- src/main/java/com/todo/service/TaskService.java
+++ src/main/java/com/todo/service/TaskService.java
@@ -1,18 +1,49 @@
 package com.todo.service;
 
-import com.todo.Task;
+import com.todo.dto.TaskResponseDTO;
+import com.todo.dto.UserResponseDTO;
+import com.todo.entity.Task;
+import com.todo.entity.User;
 import com.todo.repository.TaskRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
-    public Task createTask(Task task) {
-        return taskRepository.save(task);
+    public Task createTask(Task taskDTO) {
+        Task task = new Task();
 
+        task.setId(taskDTO.getId());
+        task.setPriority(taskDTO.getPriority());
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setStatus(taskDTO.getStatus());
+
+        taskRepository.save(task);
+        return taskDTO;
+    }
+
+    public List<TaskResponseDTO> findAll() {
+        return taskRepository.findAll().stream().map(task -> {//tüm kullanıcıları veritabanından alır
+            TaskResponseDTO taskResponseDTO = new TaskResponseDTO();
+            taskResponseDTO.setTitle(task.getTitle());
+            return taskResponseDTO;
+        }).collect(Collectors.toList());//streami listeye döndürür.
+    }
+
+    public TaskResponseDTO findById(int id) {
+        Task task = taskRepository.findById(id).get();
+
+        TaskResponseDTO taskResponseDTO = new TaskResponseDTO();
+        taskResponseDTO.setTitle(task.getTitle());
+        return taskResponseDTO;
     }
 }
+
